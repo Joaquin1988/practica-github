ee984d2519dab573bfdc57b11512f632dedcbe03
adrimartin19 <adry_inter@hotmail.com>
2015-03-09 16:47:43 +0100

    modificaciones alumno3

---
+++ GitAux.java
@@ -0,0 +1,29 @@
+package pkgGitHub;
+
+/**
+ *
+ * @author alumno: <escribe tu nombre aqui>
+ * @author alumno: <escribe tu nombre aqui>
+ * @author alumno: <Adrian Martin Jaimez>
+ * 
+ */
+public class GitAux {
+    
+    public void metodoAlumno1(){
+        System.out.println("metodo 1");
+    }
+    
+    public void metodoAlumno2(){
+        System.out.println("metodo 2");
+    }
+    
+    public void metodoAlumno3(){
+        System.out.println("metodo 3");
+        System.out.println("hola");
+    }
+    
+    public void metodoComunitario(){
+        System.out.println("Aqui escribimos todos");
+        System.out.println("adios");
+    }
+}
