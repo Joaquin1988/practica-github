39326e948e55935944cfaeeb9c7c282cfc9e6b9a
albertosg93 <albertosg93@gmail.com>
2015-03-09 16:41:32 +0100

    Cambios realizados por alumno2

---
+++ GitAux.java
@@ -0,0 +1,29 @@
+package pkgGitHub;
+
+/**
+ *
+ * @author alumno: <escribe tu nombre aqui>
+ * @author alumno: <Alberto Sánchez Gutiérrez>
+ * @author alumno: <escribe tu nombre aqui>
+ * 
+ */
+public class GitAux {
+    
+    public void metodoAlumno1(){
+        System.out.println("metodo 1");
+    }
+    
+    public void metodoAlumno2(){
+        System.out.println("metodo 2");
+	System.out.println("Hola");
+    }
+    
+    public void metodoAlumno3(){
+        System.out.println("metodo 3");
+    }
+    
+    public void metodoComunitario(){
+        System.out.println("Aqui escribimos todos");
+	System.out.println("Modificando el método comunitario");
+    }
+}
